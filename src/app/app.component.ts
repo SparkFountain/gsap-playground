16386b0e9084ef11469376ee3e86ed50abcdb6f7
Stefan Behrendt <behrendt@onexip.com>
2023-01-14 23:28:40 +0100

    first examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'gsap-playground';
+export class AppComponent implements OnInit {
+  array!: number[];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.array = new Array(25);
+
+    gsap.to('.box', {
+      scale: 0.1,
+      y: 60,
+      yoyo: true,
+      repeat: -1,
+      ease: 'power1.inOut',
+      delay: 1,
+      stagger: {
+        amount: 1.5,
+        grid: 'auto',
+        from: 'center',
+      },
+      duration: 1
+    });
+  }
 }
